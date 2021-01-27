4710225982afdd3a700df8503de72077060e0f05
Fiona Weaver <fiona.l.weaver@gmail.com>
2021-01-27 11:00:45 -0700

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,9 @@
+class Appointment < ApplicationRecord
+  belongs_to :patient
+  belongs_to :doctor
+
+  def readable_appointment_datetime
+    self.appointment_datetime.strftime('%B %d, %Y') + " at " +   self.appointment_datetime.strftime('%H:%M')
+  end
+
+end
